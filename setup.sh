ba9b0322ae63542c372996b96ee1594950e21472
Ivan Panshin <48650449+ivanpanshin@users.noreply.github.com>
2021-04-07 16:16:11 +0500

    Fixes in setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
+pip install --upgrade pip
 pip install -r requirements.txt
-git clone https://github.com/open-mmlab/mmcv.git
-cd mmcv
+git clone https://github.com/open-mmlab/mmcv.git && mmcv
 MMCV_WITH_OPS=1 pip install -e .
 cd ..
 git clone https://github.com/open-mmlab/mmdetection.git && cd mmdetection
