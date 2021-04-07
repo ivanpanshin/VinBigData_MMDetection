5f81b97f1c27f0e44c588166192cdd57c283291d
Ivan Panshin <48650449+ivanpanshin@users.noreply.github.com>
2021-04-07 16:22:44 +0500

    Fixed typos

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 pip install --upgrade pip
 pip install -r requirements.txt
-git clone https://github.com/open-mmlab/mmcv.git && mmcv
+git clone https://github.com/open-mmlab/mmcv.git && cd mmcv
 MMCV_WITH_OPS=1 pip install -e .
 cd ..
 git clone https://github.com/open-mmlab/mmdetection.git && cd mmdetection
