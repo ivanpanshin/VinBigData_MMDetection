0f28d0a15dc52726d35ad544b0ae24cc5afd270e
ivan <ivan.panshin@protonmail.com>
2021-04-08 18:58:47 +0500

    fixed setup for mmdet training

--- setup.sh
+++ setup.sh
@@ -2,10 +2,12 @@
 
 pip install --upgrade pip
 pip install -r requirements.txt
-git clone https://github.com/open-mmlab/mmcv.git && mmcv
+git clone https://github.com/open-mmlab/mmcv.git && cd mmcv
 MMCV_WITH_OPS=1 pip install -e .
 cd ..
 git clone https://github.com/open-mmlab/mmdetection.git && cd mmdetection
 pip install -r requirements/build.txt
+pip install -r requirements/optional.txt
+pip install future tensorboard
 pip install -v -e .
 cd ..
